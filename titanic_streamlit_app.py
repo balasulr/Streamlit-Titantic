e427a287df025a9737e37f6ed32cbf1690184741
Lakshmi Balasubramaniam <lrb697@gmail.com>
2025-10-27 14:40:39 -0400

    Check if have missing values

--- titanic_streamlit_app.py
+++ titanic_streamlit_app.py
@@ -11,3 +11,15 @@ data_url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/ti
 df = pd.read_csv(data_url)
 st.write(df)
 
+# See if have missing values
+st.subheader("Missing Values Overview")
+missing_counts = df.isnull().sum()
+missing_percent = (missing_counts / len(df)) * 100
+missing_df = pd.DataFrame({
+    "Missing Count": missing_counts,
+    "Missing %": missing_percent
+})
+st.dataframe(missing_df[missing_df["Missing Count"] > 0])
+
+st.write("There are missing values in the dataset")
+
