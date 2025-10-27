6960e03437ec9d2d126cfff864898bc3147b94de
Lakshmi Balasubramaniam <lrb697@gmail.com>
2025-10-27 10:05:12 -0400

    Updated ipynb file & created and ran titanic_streamlit_app.py

---
+++ titanic_streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Explore Titantic dataset with Streamlit", layout="wide")
+
+st.title("Explore Titanic Dataset with Streamlit")
+st.write("This Streamlit app explores the Titanic dataset to uncover survival insights and visualize passenger data")
