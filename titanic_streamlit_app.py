54602ad288e4ea344d452b37eaecd04e871c0ff3
Lakshmi Balasubramaniam <lrb697@gmail.com>
2025-10-27 14:03:10 -0400

    Load Titanic dataset

--- titanic_streamlit_app.py
+++ titanic_streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Explore Titantic dataset with Streamlit", layout="wide")
 
 st.title("Explore Titanic Dataset with Streamlit")
-st.write("This Streamlit app explores the Titanic dataset to uncover survival insights and visualize passenger data")
+st.subheader("This Streamlit app explores the Titanic dataset to uncover survival insights and visualize passenger data")
+
+# Load Titanic dataset from URL
+data_url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(data_url)
+st.write(df)
+
